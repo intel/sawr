3083c3dfb49cb5ced15b9bf75d702ec5afc61669
Michael McCool <michael.mccool@intel.com>
2017-04-05 16:21:34 +0900

    Update viz.sh
    Comment out rqt_graph and rqt_plot.   People can easily turn these back on if they want.

--- sawr_master/scripts/viz.sh
+++ sawr_master/scripts/viz.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 # Start some visualizers for debugging/demos
-echo "Starting rqt_graph..."
-rqt_graph &
-echo "Starting rqt_plot..."
-rqt_plot &
+#echo "Starting rqt_graph..."
+#rqt_graph &
+#echo "Starting rqt_plot..."
+#rqt_plot &
 echo "Starting rviz..."
 roslaunch sawr_navigation display.launch &
